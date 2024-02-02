92cb2853c1ab56ea625f91aeef6c8a19dfdb6cef
bahar-git <bahar1nep@gmail.com>
2024-02-01 22:56:16 -0600

    cors

--- server.js
+++ server.js
@@ -21,6 +21,16 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // --------- connect to MongoDB --------------
 
 mongoose
