657fa6c589a5bb7a760117cb978ddfdda53e5f62
bahar-git <bahar1nep@gmail.com>
2024-02-02 12:19:30 -0600

    fixing data

--- models/productModel.js
+++ models/productModel.js
@@ -1,31 +1,33 @@
 const mongoose = require("mongoose");
 const productsData = require("./data");
 
-const productSchema = mongoose.Schema({
-  category: {
-    type: String,
-    required: [true, "Please enter a category"],
+const productSchema = mongoose.Schema([
+  {
+    category: {
+      type: String,
+      required: [true, "Please enter a category"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please enter a price"],
+      default: 0,
+    },
+    strMeal: {
+      type: String,
+      required: [true, "Please enter a meal name"],
+    },
+    imgMeal: {
+      type: String,
+      required: false,
+    },
+    idMeal: {
+      type: String,
+      required: true,
+    },
   },
-  price: {
-    type: Number,
-    required: [true, "Please enter a price"],
-    default: 0,
-  },
-  strMeal: {
-    type: String,
-    required: [true, "Please enter a meal name"],
-  },
-  imgMeal: {
-    type: String,
-    required: false,
-  },
-  idMeal: {
-    type: String,
-    required: true,
-  },
-});
+]);
 
-const Products = mongoose.model("Products", productSchema);
+const Products = mongoose.model("Prod", productSchema);
 module.exports = Products;
 
-Products.insert(productsData);
+Products.insertMany(productsData);
