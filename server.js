795081c4ef68573c10ec722168db879115326507
bahar-git <bahar1nep@gmail.com>
2024-02-02 10:50:17 -0600

    cors

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URL = process.env.MONGO_URL;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/products", productRoute);
 app.use(errorMiddleware);
-app.use(cors());
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header(
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
-});
+}); */
 
 // --------- connect to MongoDB --------------
 
